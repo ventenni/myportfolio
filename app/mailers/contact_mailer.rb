8673c0195ee8f5a46dee43b1582cf19280834613
Nick Vaiente <ventenni@gmail.com>
2017-05-26 02:55:44 +0000

    added mailgun

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'nicksemailtsttests@gmail.com'
+  default to: 'nicksemailtests@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
